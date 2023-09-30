b01328187d97e9c8f9b7454905d5323638c7ee17
www.abcom.in <abcom@www.abcom.in>
2023-09-30 19:39:45 +0530

    deploy

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/getAccessToken', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
