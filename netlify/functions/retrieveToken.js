b71cc7b5659cd3c49e23f3a3fd327c289b83316d
www.abcom.in <abcom@www.abcom.in>
2023-10-03 23:10:14 +0530

    deploy

--- netlify/functions/retrieveToken.js
+++ netlify/functions/retrieveToken.js
@@ -95,17 +95,98 @@
 
 
 
-// netlify/functions/retrieveToken.js
+// // netlify/functions/retrieveToken.js
 
-const axios = require('axios');
+// const axios = require('axios');
+// const { exec } = require('child_process');
+// const fs = require('fs');
+// const cron = require('node-cron');
+
+// const TOKEN_FILE_PATH = './token.js';
+// const command = 'node ./token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js -v --keyfile ./token/node/getTokenWithServiceAccount/apt-subset-398000-ff6b648af86a.json';
+
+// let scheduled = false;
+
+// const getTokenFromCommand = () => {
+//   return new Promise((resolve, reject) => {
+//     exec(command, (error, stdout, stderr) => {
+//       if (error) {
+//         reject(error);
+//         return;
+//       }
+//       if (stderr) {
+//         reject(new Error(`Script stderr: ${stderr}`));
+//         return;
+//       }
+
+//       const lines = stdout.split('\n');
+//       const accessTokenLine = lines.find(line => line.startsWith('  "access_token":'));
+
+//       if (!accessTokenLine) {
+//         reject(new Error('No valid access_token found in the response.'));
+//         return;
+//       }
+
+//       const accessToken = accessTokenLine.split('"')[3];
+
+//       if (!accessToken) {
+//         reject(new Error('No valid access_token found in the response.'));
+//         return;
+//       }
+
+//       const tokenContent = `const token = "${accessToken}";\n\nmodule.exports = token;`;
+//       fs.writeFile(TOKEN_FILE_PATH, tokenContent, { flag: 'w' }, (err) => {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           console.log(`Token obtained and stored successfully.`);
+//           resolve(accessToken);
+//         }
+//       });
+//     });
+//   });
+// }
+
+// const retrieveToken = async () => {
+//   try {
+//     const token = await getTokenFromCommand();
+
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ accessToken: token })
+//     };
+//   } catch (error) {
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'Internal Server Error' })
+//     };
+//   }
+// };
+
+// // Add the function for serverless execution
+// exports.handler = async function(event, context) {
+//   try {
+//     const response = await retrieveToken();
+//     return response;
+//   } catch (error) {
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'Internal Server Error' })
+//     };
+//   }
+// }
+
+
+
+
+
+
+
+
+
 const { exec } = require('child_process');
-const fs = require('fs');
-const cron = require('node-cron');
 
-const TOKEN_FILE_PATH = './token.js';
 const command = 'node ./token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js -v --keyfile ./token/node/getTokenWithServiceAccount/apt-subset-398000-ff6b648af86a.json';
-
-let scheduled = false;
 
 const getTokenFromCommand = () => {
   return new Promise((resolve, reject) => {
@@ -134,20 +215,12 @@ const getTokenFromCommand = () => {
         return;
       }
 
-      const tokenContent = `const token = "${accessToken}";\n\nmodule.exports = token;`;
-      fs.writeFile(TOKEN_FILE_PATH, tokenContent, { flag: 'w' }, (err) => {
-        if (err) {
-          reject(err);
-        } else {
-          console.log(`Token obtained and stored successfully.`);
-          resolve(accessToken);
-        }
-      });
+      resolve(accessToken);
     });
   });
 }
 
-const retrieveToken = async () => {
+exports.handler = async function(event, context) {
   try {
     const token = await getTokenFromCommand();
 
@@ -155,19 +228,6 @@ const retrieveToken = async () => {
       statusCode: 200,
       body: JSON.stringify({ accessToken: token })
     };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error' })
-    };
-  }
-};
-
-// Add the function for serverless execution
-exports.handler = async function(event, context) {
-  try {
-    const response = await retrieveToken();
-    return response;
   } catch (error) {
     return {
       statusCode: 500,
