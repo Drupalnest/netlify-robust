6f05cd0d88c5a3f7369c761a68215508ecd50308
www.abcom.in <abcom@www.abcom.in>
2023-10-03 21:50:07 +0530

    deploy

--- tokenHandler.js
+++ tokenHandler.js
@@ -2,6 +2,7 @@ import Cookies from 'js-cookie';
 import token from "./token";
 
 const setInitialTokenInCookie = () => {
+    //set token
   const initialToken = token;
   
   Cookies.set('accessToken', initialToken);
