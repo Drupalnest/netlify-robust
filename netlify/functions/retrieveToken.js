888cabd57d7a2b215610a29e636b7ffc5d8c98f4
www.abcom.in <abcom@www.abcom.in>
2023-10-03 23:00:49 +0530

    deploy

--- netlify/functions/retrieveToken.js
+++ netlify/functions/retrieveToken.js
@@ -1,3 +1,100 @@
+// // netlify/functions/retrieveToken.js
+
+// const axios = require('axios');
+// const { exec } = require('child_process');
+// const fs = require('fs');
+// const cron = require('node-cron');
+
+// const TOKEN_FILE_PATH = './token.js';
+// const command = 'node ./token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js -v --keyfile ./token/node/getTokenWithServiceAccount/apt-subset-398000-ff6b648af86a.json';
+
+// let scheduled = false;
+
+// const getTokenFromCommand = () => {
+//   return new Promise((resolve, reject) => {
+//     console.log('Before exec command');
+
+//     exec(command, (error, stdout, stderr) => {
+//       if (error) {
+//         reject(error);
+//         return;
+//       }
+//       if (stderr) {
+//         reject(new Error(`Script stderr: ${stderr}`));
+//         return;
+//       }
+
+//       const lines = stdout.split('\n');
+//       const accessTokenLine = lines.find(line => line.startsWith('  "access_token":'));
+
+//       if (!accessTokenLine) {
+//         reject(new Error('No valid access_token found in the response.'));
+//         return;
+//       }
+
+//       const accessToken = accessTokenLine.split('"')[3];
+
+//       if (!accessToken) {
+//         reject(new Error('No valid access_token found in the response.'));
+//         return;
+//       }
+
+//       const tokenContent = `const token = "${accessToken}";\n\nmodule.exports = token;`;
+//       fs.writeFile(TOKEN_FILE_PATH, tokenContent, { flag: 'w' }, (err) => {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           console.log(`Token obtained and stored successfully.`);
+//           resolve(accessToken);
+//         }
+//       });
+//     });
+//   });
+// }
+
+// const retrieveToken = async () => {
+//   try {
+//     const token = await getTokenFromCommand();
+
+//     if (scheduled) {
+//       scheduled = false;
+//       cron.cancelJob('retrieveTokenJob');
+//     }
+
+//     const retrieveTokenJob = cron.schedule('*/1 * * * *', retrieveToken, { scheduled: 'retrieveTokenJob' });
+
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+// };
+
+// // Add the function for serverless execution
+// exports.handler = async function(event, context) {
+//   try {
+//     const token = await getTokenFromCommand();
+
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ message: 'Token retrieved successfully' })
+//     };
+//   } catch (error) {
+//     console.error('Error:', error);
+
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'Internal Server Error' })
+//     };
+//   }
+// }
+
+
+
+
+
+
+
+
+
 // netlify/functions/retrieveToken.js
 
 const axios = require('axios');
@@ -12,8 +109,6 @@ let scheduled = false;
 
 const getTokenFromCommand = () => {
   return new Promise((resolve, reject) => {
-    console.log('Before exec command');
-
     exec(command, (error, stdout, stderr) => {
       if (error) {
         reject(error);
@@ -56,30 +151,24 @@ const retrieveToken = async () => {
   try {
     const token = await getTokenFromCommand();
 
-    if (scheduled) {
-      scheduled = false;
-      cron.cancelJob('retrieveTokenJob');
-    }
-
-    const retrieveTokenJob = cron.schedule('*/1 * * * *', retrieveToken, { scheduled: 'retrieveTokenJob' });
-
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ accessToken: token })
+    };
   } catch (error) {
-    console.error('Error:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' })
+    };
   }
 };
 
 // Add the function for serverless execution
 exports.handler = async function(event, context) {
   try {
-    const token = await getTokenFromCommand();
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Token retrieved successfully' })
-    };
+    const response = await retrieveToken();
+    return response;
   } catch (error) {
-    console.error('Error:', error);
-
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Internal Server Error' })
