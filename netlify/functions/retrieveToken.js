d14ad92203d8dd97bfe926e0fba3653b4be3e91a
www.abcom.in <abcom@www.abcom.in>
2023-10-04 11:05:09 +0530

    deploy

--- netlify/functions/retrieveToken.js
+++ netlify/functions/retrieveToken.js
@@ -178,55 +178,106 @@
 
 
 
+// const { exec } = require('child_process');
+
+// const command = 'node ./token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js -v --keyfile ./token/node/getTokenWithServiceAccount/apt-subset-398000-ff6b648af86a.json';
+
+// exports.handler = async function(event, context) {
+//   try {
+//     console.log('Attempting to retrieve token...'); // Add this line for logging
+//     const accessToken = await new Promise((resolve, reject) => {
+//       exec(command, (error, stdout, stderr) => {
+//         if (error) {
+//           reject(error);
+//           return;
+//         }
+//         if (stderr) {
+//           reject(new Error(`Script stderr: ${stderr}`));
+//           return;
+//         }
+
+//         const lines = stdout.split('\n');
+//         const accessTokenLine = lines.find(line => line.startsWith('  "access_token":'));
+
+//         if (!accessTokenLine) {
+//           reject(new Error('No valid access_token found in the response.'));
+//           return;
+//         }
+
+//         const accessToken = accessTokenLine.split('"')[3];
+
+//         if (!accessToken) {
+//           reject(new Error('No valid access_token found in the response.'));
+//           return;
+//         }
+
+//         resolve(accessToken);
+//       });
+//     });
+
+//     console.log('Token retrieved successfully:', accessToken); // Add this line for logging
+
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ accessToken }),
+//     };
+//   } catch (error) {
+//     console.error('Error:', error); // Add this line for logging
+
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'Internal Server Error' }),
+//     };
+//   }
+// };
+
+
+
 const { exec } = require('child_process');
 
-const command = 'node ./token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js -v --keyfile ./token/node/getTokenWithServiceAccount/apt-subset-398000-ff6b648af86a.json';
+exports.handler = function(event, context, callback) {
+  const command = 'node ./token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js -v --keyfile ./token/node/getTokenWithServiceAccount/apt-subset-398000-ff6b648af86a.json';
 
-exports.handler = async function(event, context) {
-  try {
-    console.log('Attempting to retrieve token...'); // Add this line for logging
-    const accessToken = await new Promise((resolve, reject) => {
-      exec(command, (error, stdout, stderr) => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        if (stderr) {
-          reject(new Error(`Script stderr: ${stderr}`));
-          return;
-        }
-
-        const lines = stdout.split('\n');
-        const accessTokenLine = lines.find(line => line.startsWith('  "access_token":'));
-
-        if (!accessTokenLine) {
-          reject(new Error('No valid access_token found in the response.'));
-          return;
-        }
-
-        const accessToken = accessTokenLine.split('"')[3];
-
-        if (!accessToken) {
-          reject(new Error('No valid access_token found in the response.'));
-          return;
-        }
-
-        resolve(accessToken);
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Internal Server Error' })
       });
-    });
+    }
+    if (stderr) {
+      console.error(`Script stderr: ${stderr}`);
+      return callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Internal Server Error' })
+      });
+    }
 
-    console.log('Token retrieved successfully:', accessToken); // Add this line for logging
+    const lines = stdout.split('\n');
+    const accessTokenLine = lines.find(line => line.startsWith('  "access_token":'));
+    
+    if (!accessTokenLine) {
+      console.error('No valid access_token found in the response.');
+      return callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Internal Server Error' })
+      });
+    }
 
-    return {
+    const accessToken = accessTokenLine.split('"')[3];
+
+    if (!accessToken) {
+      console.error('No valid access_token found in the response.');
+      return callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Internal Server Error' })
+      });
+    }
+
+    return callback(null, {
       statusCode: 200,
-      body: JSON.stringify({ accessToken }),
-    };
-  } catch (error) {
-    console.error('Error:', error); // Add this line for logging
-
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error' }),
-    };
-  }
+      body: JSON.stringify({ accessToken })
+    });
+  });
 };
