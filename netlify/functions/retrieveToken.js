3532648b8ce9b9a4576e619073fe5adc55981c95
www.abcom.in <abcom@www.abcom.in>
2023-10-16 21:30:57 +0530

    deploy

--- netlify/functions/retrieveToken.js
+++ netlify/functions/retrieveToken.js
@@ -241,8 +241,8 @@ const { exec } = require('child_process');
 const path = require('path'); 
 
 exports.handler = function(event, context, callback) {
-  const scriptPath = path.resolve(__dirname, '/token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js');
-  const keyFilePath = path.resolve(__dirname, '/token/node/getTokenWithServiceAccount/apt-subset-398000-ff6b648af86a.json');
+  const scriptPath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js');
+  const keyFilePath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/apt-subset-398000-ff6b648af86a.json');
 
   console.log('Script Path:', scriptPath);
   console.log('Key File Path:', keyFilePath);
