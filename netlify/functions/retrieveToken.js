d44f1798c2cdee12af2e031a12f05f394ecc5353
www.abcom.in <abcom@www.abcom.in>
2023-10-17 10:23:46 +0530

    deploy

--- netlify/functions/retrieveToken.js
+++ netlify/functions/retrieveToken.js
@@ -300,14 +300,77 @@
 
 
 
+// const util = require('util');
+// const exec = util.promisify(require('child_process').exec);
+// const path = require('path');
+
+// exports.handler = async function(event, context) {
+//   try {
+//     const scriptPath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js');
+// const keyFilePath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/apt-subset-398000-ff6b648af86a.json');
+
+//     console.log('Script Path:', scriptPath);
+//     console.log('Key File Path:', keyFilePath);
+
+//     const command = `node ${scriptPath} -v --keyfile ${keyFilePath}`;
+
+//     const { stdout, stderr } = await exec(command);
+
+//     if (stderr) {
+//       console.error(`Script stderr: ${stderr}`);
+//       return {
+//         statusCode: 500,
+//         body: JSON.stringify({ error: 'Internal Server Error' })
+//       };
+//     }
+
+//     const lines = stdout.split('\n');
+//     const accessTokenLine = lines.find(line => line.startsWith('  "access_token":'));
+    
+//     if (!accessTokenLine) {
+//       console.error('No valid access_token found in the response.');
+//       return {
+//         statusCode: 500,
+//         body: JSON.stringify({ error: 'Internal Server Error' })
+//       };
+//     }
+
+//     const accessToken = accessTokenLine.split('"')[3];
+
+//     if (!accessToken) {
+//       console.error('No valid access_token found in the response.');
+//       return {
+//         statusCode: 500,
+//         body: JSON.stringify({ error: 'Internal Server Error' })
+//       };
+//     }
+
+//     return {
+//       statusCode: 200,
+//       headers: {
+//         "Access-Control-Allow-Origin": "*", 
+//         "Access-Control-Allow-Headers": "Content-Type",
+//       },
+//       body: JSON.stringify({ accessToken })
+//     };
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'Internal Server Error' })
+//     };
+//   }
+// };
+
+
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const path = require('path');
 
 exports.handler = async function(event, context) {
   try {
-    const scriptPath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js');
-const keyFilePath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/apt-subset-398000-ff6b648af86a.json');
+    const scriptPath = path.resolve(process.cwd(), 'token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js');
+    const keyFilePath = path.resolve(process.cwd(), 'token/node/getTokenWithServiceAccount/apt-subset-398000-ff6b648af86a.json');
 
     console.log('Script Path:', scriptPath);
     console.log('Key File Path:', keyFilePath);
