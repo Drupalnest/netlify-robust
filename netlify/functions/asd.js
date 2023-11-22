7b6ef3b7a78ffea36964165eaf7a2dae3323a70d
www.abcom.in <abcom@www.abcom.in>
2023-11-22 17:22:04 +0530

    deploy

--- netlify/functions/asd.js
+++ netlify/functions/asd.js
@@ -5,8 +5,8 @@ exports.handler = async (event, context) => {
   try {
     // Use process.env.LAMBDA_TASK_ROOT to get the root directory in Netlify functions
     const rootDirectory = process.env.LAMBDA_TASK_ROOT || process.cwd();
-        const scriptPath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js');
-    const keyFilePath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/inspiring-bonus-405815-b81c6343d863.json');
+    const scriptPath = path.resolve(rootDirectory, 'token', 'node', 'getTokenWithServiceAccount', 'getTokenWithServiceAccount.js');
+    const keyFilePath = path.resolve(rootDirectory, 'token', 'node', 'getTokenWithServiceAccount', 'inspiring-bonus-405815-b81c6343d863.json');
 
     console.log('Script Path:', scriptPath);
     console.log('Key File Path:', keyFilePath);
