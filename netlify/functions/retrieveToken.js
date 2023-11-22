629aa90639f24466dd17f267bc7376a166a10149
www.abcom.in <abcom@www.abcom.in>
2023-11-22 14:06:48 +0530

    deploy

--- netlify/functions/retrieveToken.js
+++ netlify/functions/retrieveToken.js
@@ -66,21 +66,156 @@
 
 
 
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
-const path = require('path');
+// const util = require('util');
+// const exec = util.promisify(require('child_process').exec);
+// const path = require('path');
 
-exports.handler = async function(event, context) {
+// exports.handler = async function(event, context) {
+//   try {
+//     const scriptPath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js');
+// const keyFilePath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/inspiring-bonus-405815-b81c6343d863.json');
+
+//     console.log('Script Path:', scriptPath);
+//     console.log('Key File Path:', keyFilePath);
+
+//     const command = `node ${scriptPath} -v --keyfile ${keyFilePath}`;
+
+//     const { stdout, stderr } = await exec(command);
+
+//     if (stderr) {
+//       console.error(`Script stderr: ${stderr}`);
+//       return {
+//         statusCode: 500,
+//         body: JSON.stringify({ error: 'Internal Server Error' })
+//       };
+//     }
+
+//     const lines = stdout.split('\n');
+//     const accessTokenLine = lines.find(line => line.startsWith('  "access_token":'));
+    
+//     if (!accessTokenLine) {
+//       console.error('No valid access_token found in the response.');
+//       return {
+//         statusCode: 500,
+//         body: JSON.stringify({ error: 'Internal Server Error' })
+//       };
+//     }
+
+//     const accessToken = accessTokenLine.split('"')[3];
+
+//     if (!accessToken) {
+//       console.error('No valid access_token found in the response.');
+//       return {
+//         statusCode: 500,
+//         body: JSON.stringify({ error: 'Internal Server Error' })
+//       };
+//     }
+
+//     return {
+//       statusCode: 200,
+//       headers: {
+//         "Access-Control-Allow-Origin": "*", 
+//         "Access-Control-Allow-Headers": "Content-Type",
+//       },
+//       body: JSON.stringify({ accessToken })
+//     };
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'Internal Server Error' })
+//     };
+//   }
+// };
+
+
+// // handler.mjs
+// import { exec } from 'child_process';
+// import path from 'path';
+// import esm from 'esm';
+
+
+
+// const esmRequire = esm(module);
+
+// const handler = async (event, context) => {
+//   try {
+//     const scriptPath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js');
+//     const keyFilePath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/inspiring-bonus-405815-b81c6343d863.json');
+
+//     console.log('Script Path:', scriptPath);
+//     console.log('Key File Path:', keyFilePath);
+
+//     const command = `node ${scriptPath} -v --keyfile ${keyFilePath}`;
+//     const { stdout, stderr } = await esmRequire('util').promisify(exec)(command);
+
+//     if (stderr) {
+//       console.error(`Script stderr: ${stderr}`);
+//       return {
+//         statusCode: 500,
+//         body: JSON.stringify({ error: 'Internal Server Error' })
+//       };
+//     }
+
+//     const lines = stdout.split('\n');
+//     const accessTokenLine = lines.find(line => line.startsWith('  "access_token":'));
+    
+//     if (!accessTokenLine) {
+//       console.error('No valid access_token found in the response.');
+//       return {
+//         statusCode: 500,
+//         body: JSON.stringify({ error: 'Internal Server Error' })
+//       };
+//     }
+
+//     const accessToken = accessTokenLine.split('"')[3];
+
+//     if (!accessToken) {
+//       console.error('No valid access_token found in the response.');
+//       return {
+//         statusCode: 500,
+//         body: JSON.stringify({ error: 'Internal Server Error' })
+//       };
+//     }
+
+//     return {
+//       statusCode: 200,
+//       headers: {
+//         "Access-Control-Allow-Origin": "*", 
+//         "Access-Control-Allow-Headers": "Content-Type",
+//       },
+//       body: JSON.stringify({ accessToken })
+//     };
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'Internal Server Error' })
+//     };
+//   }
+// };
+
+// //export { handler };
+
+
+
+const { exec } = require('child_process');
+const path = require('path');
+const esm = require('esm');
+
+const esmRequire = esm(module);
+
+exports.handler = async (event, context) => {
   try {
-    const scriptPath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js');
-const keyFilePath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/inspiring-bonus-405815-b81c6343d863.json');
+    const scriptPath = require.resolve('./token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js');
+    const keyFilePath = require.resolve('./token/node/getTokenWithServiceAccount/inspiring-bonus-405815-b81c6343d863.json');
+    
 
     console.log('Script Path:', scriptPath);
     console.log('Key File Path:', keyFilePath);
 
     const command = `node ${scriptPath} -v --keyfile ${keyFilePath}`;
-
-    const { stdout, stderr } = await exec(command);
+    const { stdout, stderr } = await esmRequire('util').promisify(exec)(command);
 
     if (stderr) {
       console.error(`Script stderr: ${stderr}`);
@@ -127,139 +262,6 @@ const keyFilePath = path.resolve(__dirname, './token/node/getTokenWithServiceAcc
     };
   }
 };
-
-
-// handler.mjs
-// import { exec } from 'child_process';
-// import path from 'path';
-// import esm from 'esm';
-
-// const esmRequire = esm(module);
-
-// const handler = async (event, context) => {
-//   try {
-//     const scriptPath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js');
-//     const keyFilePath = path.resolve(__dirname, './token/node/getTokenWithServiceAccount/apt-subset-398000-f1028d346375.json');
-
-//     console.log('Script Path:', scriptPath);
-//     console.log('Key File Path:', keyFilePath);
-
-//     const command = `node ${scriptPath} -v --keyfile ${keyFilePath}`;
-//     const { stdout, stderr } = await esmRequire('util').promisify(exec)(command);
-
-//     if (stderr) {
-//       console.error(`Script stderr: ${stderr}`);
-//       return {
-//         statusCode: 500,
-//         body: JSON.stringify({ error: 'Internal Server Error' })
-//       };
-//     }
-
-//     const lines = stdout.split('\n');
-//     const accessTokenLine = lines.find(line => line.startsWith('  "access_token":'));
-    
-//     if (!accessTokenLine) {
-//       console.error('No valid access_token found in the response.');
-//       return {
-//         statusCode: 500,
-//         body: JSON.stringify({ error: 'Internal Server Error' })
-//       };
-//     }
-
-//     const accessToken = accessTokenLine.split('"')[3];
-
-//     if (!accessToken) {
-//       console.error('No valid access_token found in the response.');
-//       return {
-//         statusCode: 500,
-//         body: JSON.stringify({ error: 'Internal Server Error' })
-//       };
-//     }
-
-//     return {
-//       statusCode: 200,
-//       headers: {
-//         "Access-Control-Allow-Origin": "*", 
-//         "Access-Control-Allow-Headers": "Content-Type",
-//       },
-//       body: JSON.stringify({ accessToken })
-//     };
-//   } catch (error) {
-//     console.error(`Error: ${error.message}`);
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ error: 'Internal Server Error' })
-//     };
-//   }
-// };
-
-// export { handler };
-
-
-
-// const { exec } = require('child_process');
-// const path = require('path');
-// const esm = require('esm');
-
-// const esmRequire = esm(module);
-
-// exports.handler = async (event, context) => {
-//   try {
-//     const scriptPath = require.resolve('./token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js');
-//     const keyFilePath = require.resolve('./token/node/getTokenWithServiceAccount/inspiring-bonus-405815-b81c6343d863.json');
-    
-
-//     console.log('Script Path:', scriptPath);
-//     console.log('Key File Path:', keyFilePath);
-
-//     const command = `node ${scriptPath} -v --keyfile ${keyFilePath}`;
-//     const { stdout, stderr } = await esmRequire('util').promisify(exec)(command);
-
-//     if (stderr) {
-//       console.error(`Script stderr: ${stderr}`);
-//       return {
-//         statusCode: 500,
-//         body: JSON.stringify({ error: 'Internal Server Error' })
-//       };
-//     }
-
-//     const lines = stdout.split('\n');
-//     const accessTokenLine = lines.find(line => line.startsWith('  "access_token":'));
-    
-//     if (!accessTokenLine) {
-//       console.error('No valid access_token found in the response.');
-//       return {
-//         statusCode: 500,
-//         body: JSON.stringify({ error: 'Internal Server Error' })
-//       };
-//     }
-
-//     const accessToken = accessTokenLine.split('"')[3];
-
-//     if (!accessToken) {
-//       console.error('No valid access_token found in the response.');
-//       return {
-//         statusCode: 500,
-//         body: JSON.stringify({ error: 'Internal Server Error' })
-//       };
-//     }
-
-//     return {
-//       statusCode: 200,
-//       headers: {
-//         "Access-Control-Allow-Origin": "*", 
-//         "Access-Control-Allow-Headers": "Content-Type",
-//       },
-//       body: JSON.stringify({ accessToken })
-//     };
-//   } catch (error) {
-//     console.error(`Error: ${error.message}`);
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ error: 'Internal Server Error' })
-//     };
-//   }
-// };
 // // // export { handler };
 
 
