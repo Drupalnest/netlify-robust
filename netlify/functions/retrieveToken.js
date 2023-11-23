4cd516b07b10bd30934d8ec8d11848c320ddf370
www.abcom.in <abcom@www.abcom.in>
2023-11-23 11:29:20 +0530

    deploy

--- netlify/functions/retrieveToken.js
+++ netlify/functions/retrieveToken.js
@@ -496,6 +496,95 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const util = require('util');
+// const { exec } = require('child_process');
+// const path = require('path');
+
+// const executeCommand = async (command, options) => {
+//   const { stdout, stderr } = await util.promisify(exec)(command, options);
+
+//   if (stderr) {
+//     console.error(`Script stderr: ${stderr}`);
+//     throw new Error('Internal Server Error');
+//   }
+
+//   return stdout;
+// };
+
+// exports.handler = async () => {
+//   try {
+//     const scriptPath = require.resolve('./token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js');
+//     const keyFilePath = require.resolve('./token/node/getTokenWithServiceAccount/inspiring-bonus-405815-b81c6343d863.json');
+    
+//     console.log('Script Path:', scriptPath);
+//     console.log('Key File Path:', keyFilePath);
+
+//     const command = `node ${scriptPath} -v --keyfile ${keyFilePath}`;
+//     const scriptStdout = await executeCommand(command);
+
+//     const lines = scriptStdout.split('\n');
+//     const accessTokenLine = lines.find(line => line.startsWith('  "access_token":'));
+
+//     if (!accessTokenLine) {
+//       console.error('No valid access_token found in the response.');
+//       throw new Error('Internal Server Error');
+//     }
+
+//     const accessToken = accessTokenLine.split('"')[3];
+
+//     if (!accessToken) {
+//       console.error('No valid access_token found in the response.');
+//       throw new Error('Internal Server Error');
+//     }
+
+//     return {
+//       statusCode: 200,
+//       headers: {
+//         'Access-Control-Allow-Origin': '*',
+//         'Access-Control-Allow-Headers': 'Content-Type',
+//       },
+//       body: JSON.stringify({ accessToken }),
+//     };
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     console.error(error.stack); // Log the full stack trace
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'Internal Server Error' }),
+//     };
+//   }
+// };
+
+
+
+
+
 const util = require('util');
 const { exec } = require('child_process');
 const path = require('path');
@@ -513,8 +602,8 @@ const executeCommand = async (command, options) => {
 
 exports.handler = async () => {
   try {
-    const scriptPath = require.resolve('./token/node/getTokenWithServiceAccount/getTokenWithServiceAccount.js');
-    const keyFilePath = require.resolve('./token/node/getTokenWithServiceAccount/inspiring-bonus-405815-b81c6343d863.json');
+    const scriptPath = path.join(__dirname, 'token', 'node', 'getTokenWithServiceAccount', 'getTokenWithServiceAccount.js');
+    const keyFilePath = path.join(__dirname, 'token', 'node', 'getTokenWithServiceAccount', 'inspiring-bonus-405815-b81c6343d863.json');
     
     console.log('Script Path:', scriptPath);
     console.log('Key File Path:', keyFilePath);
