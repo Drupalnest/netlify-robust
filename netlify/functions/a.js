3220d73cd2bae9de4d4625286acbe2d6dc3924c4
www.abcom.in <abcom@www.abcom.in>
2023-11-24 21:41:27 +0530

    sd

--- netlify/functions/a.js
+++ netlify/functions/a.js
@@ -180,7 +180,7 @@ const { GoogleToken } = require('gtoken');
 exports.handler = async function (event, context) {
   // Create a GoogleToken instance
   const gtoken = new GoogleToken({
-    keyFile: `./netlify/functions/${process.env.GOOGLE_KEY}`,
+    keyFile: './netlify/functions/inspiring-bonus-405815-b81c6343d863.json',
     email: 'apigee-acess@inspiring-bonus-405815.iam.gserviceaccount.com',
     scope: ['https://www.googleapis.com/auth/cloud-platform'],
     eagerRefreshThresholdMillis: 5 * 60 * 1000,
