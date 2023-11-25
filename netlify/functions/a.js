a35ad808849386e8363a0419717702dfab58ce56
www.abcom.in <abcom@www.abcom.in>
2023-11-25 13:41:43 +0530

    deploy

--- netlify/functions/a.js
+++ netlify/functions/a.js
@@ -268,9 +268,117 @@
 
 
 
+// // netlifyFunction.js
+// const { GoogleToken } = require('gtoken');
+// const fs = require('fs-extra');
+
+// // Provide the private key directly as a string
+// const privateKey = `-----BEGIN PRIVATE KEY-----
+// MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCzcVNeaREvR4Tf
+// nrt1Aqdpbk/58NX5iFUkEAW/E/0+vjiSOqBUkC4ijkal7q0hlt3FknEusIKVrkiQ
+// hJJTjhool3yjgHSLq+W1rpnY9i9MmDCfjdstNI7OdAMPczmB5oPKX+G/4HsnCuZ/
+// EOA+psT+/JCT2Yo/WCEKL2qwTQgA8V3AAiFgV+wDB0KpKNkJSa5QMKskrSbsBOjM
+// 6gIj8LMTGn9t6eKk7oedLV+mxG5yoPaTTCkews7zE6Qzu7RY1Duhy7rC5hy4t1TK
+// c21Zvhqccqra863e5+Loy//zKz+L22RA2t5LDRqbM81zaJChTlbRwPAmnrSRS0DX
+// qmFvn/7pAgMBAAECggEAPO5OtkCr/qdk1X/zPES17wVvr7mZFr2112R/ZwYVGHCE
+// ChdRKNW6bK5uC9sTO9QcIwHaX9W8ybGf3z685xVaCY2C24v7RxAMFJigkfzqS7Jj
+// 1FHZaiyuDylyfpUMInFRL+9F2x8RAk1fvibPVkbPnpvJRgL4wBCfS3OVYR8PCC5b
+// q3biI++oshF1Sx6KoRCpk+EzzPcxAB+sLGkdOAeLr1CWJ19UxjWiCr3zmEetWEJg
+// oZxpkbAtV4ZLCgOVuYLmKKkRZMsaJYBrdYRI12h+OlFlndcj9lEpYosjfa2Rr3f2
+// VGhPOw7xWycu1wcdRuTz7Bd5wePhZE6U+wwLXW5XLwKBgQDY77iQGzXqrM4ZbqII
+// 96OnZN/AgW+PrCygQO3PDIxEKbROVqq4/sPbJTsHODfBW704D2/TQ8dNRJaDXVoF
+// k4ZBvhvEMJQUHBw0FqSnQ6IJzD83p/cevNx2Ubhg1nK0X2pT1BcEKfFYQAA8vmHk
+// JTUAe+AAfw37XqXu9lMk+ksBPwKBgQDTwTpwUciYmoCbZyy/INvl0QbS/tKPTWlf
+// KwiTgM3upVbruqQon/G3KDJTY/f4gNwy7wqJ0SNa/Hi3QH89Lla0n9FqZjWKFf5O
+// 6257l1tYuFLJgPZQl9A9klwcxTjjMFWA29yJo4IDBJ+RoADd2PX4jNba1BVe35+D
+// SxnKIj5N1wKBgQCR9rn4PFbKz/rcodPktLsopnbm0DCAOxjLuuRHq+m081qeWzID
+// x2eAzEwrT2g0ccWfMsPLIo+3La8YNHedyOVM1t1X5Fe+MCvJ4HJpGNiuPv9KM3nS
+// 9MBh3BpzDdegBZNFVC8oXhBFkMqYFIFJQL+o07CkpNiDieucHaFUABT3gQKBgQDT
+// qhFlForh54ZoUGCNCCZmeWNN40PL1IBgjZ57+sNeKc1RwnW22fH+t+JeMs46i2wQ
+// SZsKGZAysa1k18aX/ErVL+9fYJ1h0wwqDPGZ/rhjXZP7MbqE4stX0IXciFON3NGL
+// BbpTpqodpDBwmzRijS2JbhaSFG7sL7G9xoMX5tTKbwKBgQCzhioBO/tFrgYmdNCq
+// cu1lOvZzv6As6RLXHLbI4aKuCeP7iNIEjhnNENqQhGUfRvV7X6Qi0dIO3PPmEm/e
+// lYcOkPlYczThhvAZL86Qb2QZliMrb95Wycb9K1MpJxEEoPIx6k0xSkr1DaAWFBXM
+// S2OJ3CqyWM/YsMnS/QUIZSgh8w==
+// -----END PRIVATE KEY-----`;
+
+// const email = 'apigee-acess@inspiring-bonus-405815.iam.gserviceaccount.com';
+
+// // Create a GoogleToken instance
+// const gtoken = new GoogleToken({
+//   key: privateKey, // Set keyFile to null when providing keyData directly
+//   email: email,
+//   scope: ['https://www.googleapis.com/auth/cloud-platform'],
+//   eagerRefreshThresholdMillis: 5 * 60 * 1000,
+// });
+
+// // Function to get a token
+// const getToken = () =>
+//   new Promise((resolve, reject) => {
+//     gtoken.getToken((err, tokens) => {
+//       if (err) {
+//         reject(err);
+//         return;
+//       }
+//       resolve(tokens);
+//     });
+//   });
+
+// exports.handler = async function (event, context) {
+
+
+
+//   const headers = {
+//     'Access-Control-Allow-Origin': '*',
+//     'Access-Control-Allow-Headers': 'Content-Type',
+//   };
+
+//   if (event.httpMethod === 'OPTIONS') {
+//     return {
+//       statusCode: 200,
+//       headers,
+//       body: '',
+//     };
+//   }
+//   try {
+//     // Get the initial token
+//     const initialToken = await getToken();
+//     console.log('Initial Token:', initialToken);
+
+//     // Your logic using the initial token
+
+//     // Check if token needs refreshing based on expires_in
+//     const expirationTime = initialToken.expires_in * 1000; // Convert seconds to milliseconds
+//     const currentTime = Date.now();
+
+//     if (currentTime + expirationTime < gtoken.eagerRefreshThresholdMillis) {
+//       // Refresh the token
+//       const refreshedToken = await getToken();
+//       console.log('Refreshed Token:', refreshedToken);
+
+//       // Your logic using the refreshed token
+//     }
+
+//     // Continue with your application logic
+
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({  accessToken: initialToken.access_token }),
+//     };
+//   } catch (error) {
+//     console.error('Error:', error);
+
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ success: false, error: error.message || 'Internal Server Error' }),
+//     };
+//   }
+// };
+
+
+
 // netlifyFunction.js
 const { GoogleToken } = require('gtoken');
-const fs = require('fs-extra');
 
 // Provide the private key directly as a string
 const privateKey = `-----BEGIN PRIVATE KEY-----
@@ -306,7 +414,7 @@ const email = 'apigee-acess@inspiring-bonus-405815.iam.gserviceaccount.com';
 
 // Create a GoogleToken instance
 const gtoken = new GoogleToken({
-  key: privateKey, // Set keyFile to null when providing keyData directly
+  key: privateKey,
   email: email,
   scope: ['https://www.googleapis.com/auth/cloud-platform'],
   eagerRefreshThresholdMillis: 5 * 60 * 1000,
@@ -325,11 +433,9 @@ const getToken = () =>
   });
 
 exports.handler = async function (event, context) {
-
-
-
+  // Add CORS headers to the response
   const headers = {
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': '*', // Change this to your specific domain in production
     'Access-Control-Allow-Headers': 'Content-Type',
   };
 
@@ -340,6 +446,7 @@ exports.handler = async function (event, context) {
       body: '',
     };
   }
+
   try {
     // Get the initial token
     const initialToken = await getToken();
@@ -363,13 +470,15 @@ exports.handler = async function (event, context) {
 
     return {
       statusCode: 200,
-      body: JSON.stringify({  accessToken: initialToken.access_token }),
+      headers, // Include CORS headers in the response
+      body: JSON.stringify({ accessToken: initialToken.access_token }),
     };
   } catch (error) {
     console.error('Error:', error);
 
     return {
       statusCode: 500,
+      headers, // Include CORS headers in the response
       body: JSON.stringify({ success: false, error: error.message || 'Internal Server Error' }),
     };
   }
