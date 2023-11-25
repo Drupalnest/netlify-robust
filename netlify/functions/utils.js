74bd4e0a1efe2518551c52987c657b253a0adbd9
www.abcom.in <abcom@www.abcom.in>
2023-11-25 13:47:18 +0530

    deploy

--- netlify/functions/utils.js
+++
@@ -1,8 +0,0 @@
-// utils.js
-const { exec } = require('child_process');
-
-module.exports.executeScript = function (command, callback) {
-  exec(command, (error, stdout, stderr) => {
-    callback(error, stdout, stderr);
-  });
-};
